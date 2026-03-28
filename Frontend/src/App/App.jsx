4b892180258ae68711fd18ac02de4543d12289bd
Pranav Zhawar <pranavzhawar@gmail.com>
2026-03-29 00:35:34 +0530

    setup

---
+++ Frontend/src/App/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <><main className="h-screen w-full bg-gray-950 flex gap-4 p-4 items-center justify-center" >
+        {/* <form
+          onSubmit={handleJoin}
+          className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="Enter your username"
+            className="p-2 rounded-lg bg-gray-800 text-white"
+            name="username"
+          />
+          <button
+            className="p-2 rounded-lg bg-amber-50 text-gray-950 font-bold"
+          >
+            Join
+          </button>
+        </form> */}
+      </main>
+      
+    </>
+  )
+}
+
+export default App
